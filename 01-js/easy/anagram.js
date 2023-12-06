99d45f1c36c40d0dbeaf2e4febcaa337847882ef
Gaurav Sharma <gaurav89830@gmail.com>
2023-12-06 23:42:59 +0530

    testing

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,7 +5,16 @@
 */
 
 function isAnagram(str1, str2) {
+  if (str1.length !== str2.length) {
+    return false;
+  }
+  str1 = str1.toLowerCase().split("").sort().join("");
+  str2 = str2.toLowerCase().split("").sort().join("");
 
+  console.log(str1);
+  console.log(str2);
+  console.log("was");
+  return str1 === str2;
 }
 
 module.exports = isAnagram;
